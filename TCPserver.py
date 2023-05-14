c19ae9b543f7536b212b5f91711ea49bbbe25391
Retr0-t <88282611+Retr0-t@users.noreply.github.com>
2023-05-14 11:55:58 +0700

    Update TCPserver.py
    penyesuaian agar bisa membuka localhost pada port 8080  ke direktori yang ada index.html

--- TCPserver.py
+++ TCPserver.py
@@ -1,94 +1,51 @@
-import os
 import socket
+import os
 
-# Server address and port
-HOST = '127.0.0.1'
-PORT = 8080
+# Mengatur alamat dan port server
+alamat_server = 'localhost'  # Alamat IP loopback untuk server lokal
+port_server = 8080  # Port yang akan digunakan oleh server
 
-# Response status codes
-STATUS_OK = '200 OK'
-STATUS_NOT_FOUND = '404 Not Found'
+# Mendapatkan direktori kerja saat ini
+current_directory = os.getcwd()
 
-# Response headers
-HEADERS_OK = {
-    'Content-Type': 'text/html; encoding=utf8',
-    'Content-Length': 0,
-    'Connection': 'close',
-}
-HEADERS_NOT_FOUND = {
-    'Content-Type': 'text/html; encoding=utf8',
-    'Content-Length': 0,
-    'Connection': 'close',
-}
+# Menentukan lokasi file "index.html"
+index_html_path = os.path.join(current_directory, 'C:\\Users\\ASUS\\Downloads\\SEMESTER 4\\JARKOM PRAK\\TUBES SERVER\\index.html')
 
-class MyTCPHandler:
-    def __init__(self, sock):
-        self.sock = sock
+# Membaca isi file "index.html"
+with open(index_html_path, 'r') as file:
+    content = file.read()
 
-    def handle(self):
-        # Receive data from client
-        data = self.sock.recv(1024)
+# Membuat objek socket TCP
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-        # Parse HTTP request
-        try:
-            request = data.decode().split('\r\n')
-            method, path, version = request[0].split()
-        except:
-            return
+# Mengaitkan socket dengan alamat dan port
+sock.bind((alamat_server, port_server))
 
-        # Check if method is GET
-        if method != 'GET':
-            return
+# Mendengarkan koneksi masuk
+sock.listen(1)  # Hanya satu koneksi yang dapat di-queue
 
-        # Build file path
-        file_path = os.path.join(os.getcwd(), path.lstrip('/'))
+print("Server TCP siap untuk menerima koneksi.")
 
-        # Check if file exists
-        if not os.path.isfile(file_path):
-            self.send_response(STATUS_NOT_FOUND, HEADERS_NOT_FOUND)
-            return
+while True:
+    # Menerima koneksi dari client
+    conn, addr = sock.accept()
 
-        # Read file
-        with open(file_path, 'rb') as f:
-            content = f.read()
+    print(f"Menerima koneksi dari: {addr[0]}:{addr[1]}")
 
-        # Build response headers
-        headers = HEADERS_OK.copy()
-        headers['Content-Length'] = len(content)
+    # Menerima data dari client
+    data = conn.recv(1024).decode('utf-8')
+    print(f"Data yang diterima: {data}")
 
-        # Send response
-        self.send_response(STATUS_OK, headers, content)
+    # Memeriksa jika permintaan adalah GET /index.html
+    if "GET /" in data:
+        # Mengirimkan konten HTML ke client
+        response = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n" + content
+    else:
+        # Mengirimkan respons 404 - Page Not Found
+        response = "HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\n\r\n404 - Page Not Found"
 
-    def send_response(self, status, headers, content=b''):
-        # Build response message
-        response = f'HTTP/1.1 {status}\r\n'
-        for key, value in headers.items():
-            response += f'{key}: {value}\r\n'
-        response += '\r\n'
-        response = response.encode() + content
+    # Mengirimkan respons ke client
+    conn.sendall(response.encode('utf-8'))
 
-        # Send response to client
-        self.sock.sendall(response)
-
-if __name__ == '__main__':
-    # Create TCP socket
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    # Set socket options to reuse address and port
-    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-    # Bind to address and port
-    server_socket.bind((HOST, PORT))
-
-    # Listen for incoming connections
-    server_socket.listen()
-
-    print(f'Server listening on {HOST}:{PORT}...')
-
-    while True:
-        # Accept incoming connection
-        client_socket, client_address = server_socket.accept()
-
-        # Handle incoming connection in a separate thread
-        handler = MyTCPHandler(client_socket)
-        handler.handle()
+    # Menutup koneksi dengan client
+    conn.close()
